00dd89b9c8f61830fbc287a648f3bcf24c976366
Dany <aliend.bcn.photography@gmail.com>
2026-09-03 20:18:21 +0200

    content(modulo-1): add Gen COMT (Val158Met) Warrior vs Worrier concept (seccion 1.4)

--- src/data/index-models.ts
+++ src/data/index-models.ts
@@ -27,6 +27,7 @@ import sinaptogenesisJson from './concepts/sinaptogenesis-y-mielinizacion.json';
 import neuroinflamacionJson from './concepts/neuroinflamacion.json';
 import estresOxidativoJson from './concepts/estres-oxidativo.json';
 import introGeneticaJson from './concepts/introduccion-genetica.json';
+import comtJson from './concepts/comt-val158met.json';
 import ltpJson from './concepts/plasticidad-sinaptica.json';
 
 export const categorias: Categoria[] = categoriesJson as Categoria[];
@@ -56,6 +57,7 @@ export const conceptos: Concepto[] = [
   neuroinflamacionJson as Concepto,
   estresOxidativoJson as Concepto,
   introGeneticaJson as Concepto,
+  comtJson as Concepto,
   ltpJson as Concepto,
 ];
 
