577ebe1c868a753833e070ae9e29a1ad759dcca7
Dany <aliend.bcn.photography@gmail.com>
2026-09-03 18:56:07 +0200

    content(modulo-1): add concept neuroanatomia-funcional (seccion 1.1)

--- src/data/index-models.ts
+++ src/data/index-models.ts
@@ -2,10 +2,14 @@ import { Sustancia, Concepto, Categoria } from '@/types/neuroatlas-models';
 import categoriesJson from './categories/categories.json';
 import bacopaJson from './substances/bacopa-monnieri.json';
 import ltpJson from './concepts/plasticidad-sinaptica.json';
+import neuroanatomiaJson from './concepts/neuroanatomia-funcional.json';
 
 export const categorias: Categoria[] = categoriesJson as Categoria[];
 export const sustancias: Sustancia[] = [bacopaJson as Sustancia];
-export const conceptos: Concepto[] = [ltpJson as Concepto];
+export const conceptos: Concepto[] = [
+  neuroanatomiaJson as Concepto,
+  ltpJson as Concepto,
+];
 
 export const getSustanciaBySlug = (slug: string): Sustancia | undefined => {
   return sustancias.find((s) => s.slug === slug);
