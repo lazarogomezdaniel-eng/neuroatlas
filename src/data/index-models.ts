fe11742be6ea1b1a71e5980b4ec05a1323162f29
Dany <aliend.bcn.photography@gmail.com>
2026-09-04 16:39:35 +0200

    content(modulo-2): add Vitamina B7 (Biotina, Mielinizacion, Carboxilasas y Avidina) concept (seccion 2.1 - orden 40)

--- src/data/index-models.ts
+++ src/data/index-models.ts
@@ -65,6 +65,7 @@ import vitaminaB2Json from './concepts/vitamina-b2-riboflavina.json';
 import vitaminaB3Json from './concepts/vitamina-b3-niacina.json';
 import vitaminaB5Json from './concepts/vitamina-b5-acido-pantotenico.json';
 import vitaminaB6Json from './concepts/vitamina-b6-piridoxina.json';
+import vitaminaB7Json from './concepts/vitamina-b7-biotina.json';
 
 export const categorias: Categoria[] = categoriesJson as Categoria[];
 export const sustancias: Sustancia[] = [bacopaJson as Sustancia];
@@ -127,6 +128,7 @@ export const conceptos: Concepto[] = [
   vitaminaB3Json as Concepto,
   vitaminaB5Json as Concepto,
   vitaminaB6Json as Concepto,
+  vitaminaB7Json as Concepto,
 ];
 
 export const getSustanciaBySlug = (slug: string): Sustancia | undefined => {
